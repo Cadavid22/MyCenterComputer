ae8b08b6b4720d24c03fec6287576fc0a07b71ff
Jorge Cadavid <cadavidjorge10@gmail.com>
2023-06-10 15:40:12 -0500

    Add project files.

---
+++ CenterComputer.Backend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using CenterComputer.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CenterComputer.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        internal readonly object MyCenterComputer;
+        internal readonly object MyComputer;
+
+        public DataContext(DbContextOptions<DataContext> op) : base(op)
+        {
+        }
+
+        public DbSet<MyCenterComputer> CenterComputers { get; set; }
+
+    }
+}
+}
